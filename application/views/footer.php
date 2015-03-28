bca524603298788b67eb040c5fc7fa88a2927f04
Jordi Bagot <u1909821@campus.udg.edu>
2015-03-28 13:25:17 +0100

    Nous assets i 3 vistes noves

---
+++ application/views/footer.php
@@ -0,0 +1,11 @@
+</body>
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                HackathonGi - Admin/BackOffice Team 2015
+            </div>
+        </div>
+    </div>
+</footer>
+</html>
