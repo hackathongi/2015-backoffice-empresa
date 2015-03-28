e721e44ce5900f6d480f133ecda004cbf8d8d24f
lluis gifra <lluisgifra@gmail.com>
2015-03-28 15:27:02 +0100

    Login

---
+++ application/views/login_view.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: danie_000
+ * Date: 28/03/2015
+ * Time: 11:52
+ */
+
+?>
+
+<form action="http://apisocial.wallyjobs.com/login" method="post">
+    <div class="form-group">
+        <label for="userEmail">Email</label>
+        <input type="email" class="form-control" id="userEmail" placeholder="Email">
+    </div>
+    <div class="form-group">
+        <label for="userPassword">Password</label>
+        <input type="password" class="form-control" id="userPassword" placeholder="Password">
+    </div>
+    <button type="submit" class="btn btn-default">Login</button>
+</form>
