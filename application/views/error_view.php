f739daebd6978aac807768750f64a6da6dcfc02d
lluis gifra <lluisgifra@gmail.com>
2015-03-28 17:42:45 +0100

    Add error_view Modify jobList_view

---
+++ application/views/error_view.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: danie_000
+ * Date: 28/03/2015
+ * Time: 17:35
+ */
+?>
+
+<div class="section">
+    <div class="container">
+        <?php echo $error; ?>
+    </div>
+</div>
