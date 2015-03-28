fd3992d11ce4aac7e23c113d4a4c8949316a844e
ibianchi <ivaanbianchi@gmail.com>
2015-03-28 17:17:30 +0100

    Stylesheets

--- application/views/header.php
+++ application/views/header.php
@@ -2,22 +2,19 @@
 <html lang="es">
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php setlocale(LC_ALL,"es_ES"); ?>
     <title>WallyJobs</title>
-
-    <script type="text/javascript" src="http://cdnjs.cloudflare.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
-    <script type="text/javascript" src="http://netdna.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
-    <link href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.3.0/css/font-awesome.min.css"
-          rel="stylesheet" type="text/css">
-    <link rel="stylesheet" href="2015-stylesheet/demo/css/hackajobs-stylesheet.css">
-
-    <link type='text/css' href="<?php echo base_url(); ?>/assets/css/bootstrap.min.css" rel='stylesheet' />
-    <link type='text/css' href="<?php echo base_url(); ?>/assets/css/font-awesome.min.css" rel="stylesheet">
+	
+	<link href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.3.0/css/font-awesome.min.css"
+	rel="stylesheet" type="text/css">
     <link type='text/css' rel="stylesheet" href="//code.jquery.com/ui/1.11.1/themes/smoothness/jquery-ui.css">
+	<link rel="stylesheet" href="<?php echo base_url(); ?>../application/assets/css/hackajobs-stylesheet.css">
 
-    <script src="<?php echo base_url(); ?>/assets/js/jquery-1.10.2.min.js" type="text/javascript"></script>  
+	<script type="text/javascript" src="http://netdna.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="http://cdnjs.cloudflare.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
     <script src="//code.jquery.com/ui/1.11.1/jquery-ui.js"></script>
+	
+	
 </head>
 <body class="col-md-12 col-lg-12 col-sm-12 col-xs-12 container">
