b8603f5258c95ca6ce0261aec9385d4af815e411
ibianchi <ivaanbianchi@gmail.com>
2015-03-28 22:52:31 +0100

    Using curl

--- application/models/User_model.php
+++ application/models/User_model.php
@@ -54,7 +54,18 @@ class User_model extends CI_Model{
 
             $url = $base_url."/".$provider."/".$user1."/".$user2;
 
-            $response = http_get($url, array("timeout"=>1), $info);
+            // Get cURL resource
+            $curl = curl_init();
+            // Set some options - we are passing in a useragent too here
+            curl_setopt_array($curl, array(
+                CURLOPT_RETURNTRANSFER => 1,
+                CURLOPT_URL => $url,
+            ));
+            $info = curl_exec($curl);
+            // Close request to clear up some resources
+            curl_close($curl);
+
+            echo $info;
 
             $jsonList = json_decode($info);
 
