6be400d8369666011370c572c1e5ece17047a0f3
ibianchi <ivaanbianchi@gmail.com>
2015-03-28 15:35:29 +0100

    Model major queries

--- application/models/Application_model.php
+++ application/models/Application_model.php
@@ -37,4 +37,27 @@ class Application_model extends CI_Model {
             return false;
         }
     }
+
+    /**
+     * Recupera la url d'un cv
+     * @param Array $application_data
+     * @return if exists return array, else false
+     */
+    function get_cv($application_data)
+    {
+        try{
+            $this->db->get_where('tbl_application.cv_url');
+            $query = $this->db->get_where('tbl_application', $application_data);
+            if ($query->num_rows() > 0) {
+                return $query->row();
+            }
+            else {
+                return false;
+            }
+        } catch (Exception $e) {
+            return false;
+        }
+    }
+
+
 }
