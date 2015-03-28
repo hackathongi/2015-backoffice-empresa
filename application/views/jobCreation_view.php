27ffc650f7e94643c67b157217376bf1a1ddea7c
ibianchi <ivaanbianchi@gmail.com>
2015-03-28 16:30:26 +0100

    Job Creation

--- application/views/jobCreation_view.php
+++ application/views/jobCreation_view.php
@@ -47,32 +47,52 @@ $dataIni = date("Y-m-d");
                     </li>
                 </ul>
                 <h1>Alta oferta</h1>
-                <form action="<?php echo base_url(); ?>/job/create" method="post">
+                <div>
+                    <?php echo validation_errors(); ?>
+                </div>
+                <form action="<?php echo base_url(); ?>job/create" method="post">
                     <div class="form-group">
                         <label class="control-label" for="exampleInputEmail1">Títol</label>
                         <input class="form-control" id="exampleInputEmail1" placeholder="Introdueix el títol de l'oferta"
-                               type="email">
+                               type="text" name="title">
                     </div>
                     <div class="form-group">
-                        <label class="control-label" for="exampleInputPassword1">Descripció</label>
-                        <textarea class="form-control" rows="3" placeholder="Descriu breument l'oferta"></textarea>
+                        <label class="control-label" for="jobDescription">Descripció</label>
+                        <textarea id="jobDescription" class="form-control" name="description" rows="3" placeholder="Descriu breument l'oferta"></textarea>
                     </div>
                     <div class="form-group">
-                        <label class="control-label" for="exampleInputEmail1">Data fi</label>
+                        <label class="control-label" for="jobDateIni">Data ini</label>
                         <div>
-                            <input type="date">
+                            <input id="jobDateIni" name="start_date" type="date" value="<?php echo $dataIni; ?>">
                         </div>
                     </div>
                     <div class="form-group">
-                        <label class="control-label" for="exampleInputEmail1">Població</label>
-                        <input class="form-control" id="exampleInputEmail1" placeholder="Introdueix el títol de l'oferta"
-                               type="email">
+                        <label class="control-label" for="jobDateFin">Data fi</label>
+                        <div>
+                            <input id="jobDateFin" name="end_date" type="date" >
+                        </div>
                     </div>
                     <div class="form-group">
-                        <label class="control-label" for="exampleInputEmail1">Foto oferta</label>
-                        <input class="file" id="input-1" type="file">
+                        <label class="control-label" for="jobCity">Població</label>
+                        <input class="form-control" id="jobCity" name="city" placeholder="Poblacio"
+                               type="text">
                     </div>
-                    <button type="submit" class="btn btn-danger">&lt;&lt; Tornar inici</button>
+                    <div class="form-group">
+                        <label class="control-label" for="jobLongitude">Latitud</label>
+                        <input class="form-control" id="jobLongitude" name="latitude" placeholder="Latitud"
+                               type="text">
+                    </div>
+                    <div class="form-group">
+                        <label class="control-label" for="jobLatitude">Longitud</label>
+                        <input class="form-control" id="jobLatitude" name="longitude" placeholder="Longitud"
+                               type="text">
+                    </div>
+                    <div class="form-group">
+                        <label class="control-label" for="input-1">Foto oferta</label>
+                        <input class="file" id="input-1" name="picture_url" type="file">
+                    </div>
+                    <a href="<?php echo base_url(); ?>" class="btn btn-danger">&lt;&lt; Tornar inici</a>
+
                     <button type="submit" class="btn btn-primary">Publicar oferta a Facebook</button>
                 </form>
             </div>
